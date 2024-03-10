59f850dda35d5d48b0001f912be79d1e2ab732a7
Stephen Stolk <shikharburman2@gmail.com>
2024-03-10 23:19:45 +0530

    Added OCR uploading page.

--- webcomponents/client/src/components/Foreground.jsx
+++ webcomponents/client/src/components/Foreground.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Board2 from './Board2'
+import TesseractOCR from '../pages/TesseractOCR'
 
 const Foreground = () => {
   return (
     <>
-      <Board2 />
+      {/* <Board2 /> */}
+      <TesseractOCR />
+      {/* onChange={({target: {files}}) => {
+                            files[0] && setFileName(files[0].name)
+                            if(files) {
+                                setImage(URL.createObjectURL(files[0]))
+                            }
+                        }} */}
     </>
   )
 }

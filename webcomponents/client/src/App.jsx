733ef83a9a76b3dd4f206a6f9783c43d4d6b0404
Rishi Bharadwaj Sai Lella <143260938+rishibharadwajsai@users.noreply.github.com>
2024-03-13 17:40:46 +0530

    Update App.jsx

--- webcomponents/client/src/App.jsx
+++ webcomponents/client/src/App.jsx
@@ -3,16 +3,28 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Board2 from './components/Board2'
 import Foreground from './components/Foreground'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TesseractOCR from '../pages/TesseractOCR';
 
 
 
 const App = () => {
+  // return (
+  //   <>
+  //     <Navbar />
+  //     <Foreground />
+  //   </>
+  // )
   return (
-    <>
-      <Navbar />
-      <Foreground />
-    </>
-  )
-}
+        <Router>
+            <div>
+                <Navbar />
+                <Switch>
+                    <Route path="/" component={Foreground} />
+                </Switch>
+            </div>
+        </Router>
+    );
+};
 
 export default App

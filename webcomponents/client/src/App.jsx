1b33646b27fcb484b08d470ce6b47e3fa5bdaddc
Rishi Bharadwaj Sai Lella <143260938+rishibharadwajsai@users.noreply.github.com>
2024-03-13 18:25:51 +0530

    Update App.jsx

--- webcomponents/client/src/App.jsx
+++ webcomponents/client/src/App.jsx
@@ -1,19 +1,44 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Navbar from './components/Navbar'
-import Board2 from './components/Board2'
-import Foreground from './components/Foreground'
+// import React from 'react'
+// import Navbar from './components/Navbar'
+// import Board2 from './components/Board2'
+// import Foreground from './components/Foreground'
+// import TesseractOCR from './pages/TesseractOCR';
+
+
+
+// const App = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <Foreground />
+//     </>
+//   )
+// };
+
+// export default App
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Foreground from './components/Foreground';
+import Board2 from './components/Board2';
 import TesseractOCR from './pages/TesseractOCR';
-
-
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Foreground />
-    </>
-  )
-};
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path='/board2' component={Board2} />
+          <Route path='/tesseract' component={TesseractOCR} />
+          <Route path='/' component={Foreground} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 
-export default App
+export default App;

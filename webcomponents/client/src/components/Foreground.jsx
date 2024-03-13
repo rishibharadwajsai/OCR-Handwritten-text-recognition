95538967caa05cff599fd5e53e1e5a879503b691
Rishi Bharadwaj Sai Lella <143260938+rishibharadwajsai@users.noreply.github.com>
2024-03-13 18:17:37 +0530

    Update Foreground.jsx

--- webcomponents/client/src/components/Foreground.jsx
+++ webcomponents/client/src/components/Foreground.jsx
@@ -1,42 +1,21 @@
-// import React from 'react'
-// import Board2 from './Board2'
-// import TesseractOCR from '../pages/TesseractOCR'
-
-// const Foreground = () => {
-//   return (
-//     <>
-//       <Board2 />
-// {/*       <TesseractOCR /> */}
-//       {/* onChange={({target: {files}}) => {
-//                             files[0] && setFileName(files[0].name)
-//                             if(files) {
-//                                 setImage(URL.createObjectURL(files[0]))
-//                             }
-//                         }} */}
-//     </>
-//   )
-// }
-
-// export default Foreground
-
-
-import React, { useState } from 'react';
-import Board2 from './Board2';
-import TesseractOCR from '../pages/TesseractOCR';
+import React from 'react'
+import Board2 from './Board2'
+import TesseractOCR from '../pages/TesseractOCR'
 
 const Foreground = () => {
-  const [isBoard2, setIsBoard2] = useState(true);
-
-  const switchComponent = () => {
-    setIsBoard2(!isBoard2);
-  };
-
   return (
     <>
-      {isBoard2 ? <Board2 /> : <TesseractOCR />}
-      <button onClick={switchComponent}>Switch</button>
+      <Board2 />
+{/*       <TesseractOCR /> */}
+      {/* onChange={({target: {files}}) => {
+                            files[0] && setFileName(files[0].name)
+                            if(files) {
+                                setImage(URL.createObjectURL(files[0]))
+                            }
+                        }} */}
     </>
-  );
-};
+  )
+}
 
-export default Foreground;
+export default Foreground
+
